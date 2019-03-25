2b9600179a9de2ff34b2e965b43d2a7110052587
Kamran Siddique <kamranprogrammer28@gmail.com>
2019-03-25 23:58:32 +0530

    removed extra code

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import arr from './data'
 import User from './components/userComponent'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      numberOfLikes : 0
-    }
-    this.increaseLike = this.increaseLike.bind(this)
-  }
-  increaseLike(){
-    this.setState({
-      numberOfLikes : this.state.numberOfLikes + 1
-    })
-  }
   render() {
     console.log(arr)
     var elements = arr.map(item=>{
